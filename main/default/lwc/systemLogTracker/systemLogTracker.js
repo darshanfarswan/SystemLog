d4fabfc1417fb0e3a2e1179ad81bf18944f5e2e5
Darshan Farswan <darshan.farswan@makepositive.com>
2020-04-30 12:45:24 +0530

    Adding Application / Module and comments

--- main/default/lwc/systemLogTracker/systemLogTracker.js
+++ main/default/lwc/systemLogTracker/systemLogTracker.js
@@ -4,10 +4,16 @@ import getRecords from '@salesforce/apex/SystemLogUtils.getRecords';
 
 export default class SystemLogTracker extends LightningElement {
     @track channelName = '/data/System_Log__ChangeEvent';
-    @track systemLogs;
-    @track numberOfRecords = 20;
-
-    subscribeEvents() {
+    @track allSystemLogs;
+    @track filteredSystemLogs;
+    @track isSubscribed = false;
+    @track showSuccess = true;
+    @track showInfo = true;
+    @track showWarning = true;
+    @track showError = true;
+    @track lastRefreshDateTime;
+    
+    connectedCallback() {
         const messageCallback = (response) => {
             console.log('New Log Added.')
             console.log(JSON.stringify(response));
@@ -42,29 +48,33 @@ export default class SystemLogTracker extends LightningElement {
                     "Record_Id__c" : cdcData.data.payload.Record_Id__c,
                     "Record__c" : cdcData.data.payload.Record__c,
                     "Source__c" : cdcData.data.payload.Source__c,
+                    "Application__c" : cdcData.data.payload.Application__c,
+                    "Module__c" : cdcData.data.payload.Module__c,
                     "Stack_Trace__c" : cdcData.data.payload.Stack_Trace__c,
                     "IconName" : iconName,
                     "CardClass" : cardClass
                 });
+                this.filterLogs();
+                this.lastRefreshDateTime = new Date();
             }
         }
 
-        subscribe(this.channelName, -1, messageCallback).then(response => {
-            // Response contains the subscription information on successful subscribe call
-            console.log('Successfully subscribed to : ', JSON.stringify(response.channel));
-            //this.subscription = response;
-            //this.toggleSubscribeButton(true);
-        });
+        if (this.isSubscribed == false) {
+            subscribe(this.channelName, -1, messageCallback).then(response => {
+                console.log('Successfully subscribed to : ', JSON.stringify(response.channel));
+                this.isSubscribed = true
+            });
+        }
     }
 
-    @wire (getRecords, {numberOfRecords : '$numberOfRecords'})
+    @wire (getRecords, {})
     wiredRecords ({error, data}) {
         console.log('wiredRecords');
         console.log(error);
         console.log(data);
 
         if (data) {
-            var systemLogs = [];
+            var allSystemLogs = [];
             for (var index in data) {
                 var record = data[index];
                 var iconName = "action:approval";
@@ -87,7 +97,7 @@ export default class SystemLogTracker extends LightningElement {
                         cardClass = "slds-notify slds-notify_alert slds-theme_alert-texture slds-theme_error";
                         break; 
                 }
-                systemLogs.push({
+                allSystemLogs.push({
                     "Id" : record.Id,
                     "Name" : record.Name,
                     "Log_Level__c" : record.Log_Level__c,
@@ -95,15 +105,60 @@ export default class SystemLogTracker extends LightningElement {
                     "Record_Id__c" : record.Record_Id__c,
                     "Record__c" : record.Record__c,
                     "Source__c" : record.Source__c,
+                    "Application__c" : record.Application__c,
+                    "Module__c" : record.Module__c,
                     "Stack_Trace__c" : record.Stack_Trace__c,
                     "IconName" : iconName,
                     "CardClass" : cardClass
                 });
             }
-            this.systemLogs = systemLogs;
+            this.lastRefreshDateTime = new Date();
+            this.allSystemLogs = allSystemLogs;
+            this.filteredSystemLogs = allSystemLogs;
+            console.log(this.filteredSystemLogs);
         } else if (error) {
             console.log(error);
         }
+    }
+
+    toggleLevel(event) {
+        console.log(event);
+        console.log(event.target.label);
+        console.log(event.target.variant);
+        var logLevel = event.target.label;
+        event.target.variant = event.target.variant == 'neutral' ? 'success' : 'neutral';
+        if (logLevel == 'Success') {
+            this.showSuccess = this.showSuccess == true ? false : true;
+        } else if (logLevel == 'Info') {
+            this.showInfo = this.showInfo == true ? false : true;
+        } else if (logLevel == 'Warning') {
+            this.showWarning = this.showWarning == true ? false : true;
+        } else if (logLevel == 'Error') {
+            this.showError = this.showError == true ? false : true;
+        }
+
+        this.filterLogs();
+    }
+
+    filterLogs() {
+        var filteredSystemLogs = [];
+        for (var index in this.allSystemLogs) {
+            var systemLog = this.allSystemLogs[index];
+            if(systemLog.Log_Level__c == 'Success' && this.showSuccess) {
+                console.log('Adding Success');
+                filteredSystemLogs.push(systemLog);
+            } else if(systemLog.Log_Level__c == 'Info' && this.showInfo) {
+                console.log('Adding Info');
+                filteredSystemLogs.push(systemLog);
+            } else if(systemLog.Log_Level__c == 'Warning' && this.showWarning) {
+                console.log('Adding Warning');
+                filteredSystemLogs.push(systemLog);
+            } else if(systemLog.Log_Level__c == 'Error' && this.showError) {
+                console.log('Adding Error');
+                filteredSystemLogs.push(systemLog);
+            }
+        }
+        this.filteredSystemLogs = filteredSystemLogs;
     }
 
     openSystemLog(event) {
@@ -114,10 +169,10 @@ export default class SystemLogTracker extends LightningElement {
     }
 
     addToSystemLogs(systemLog) {
-        var systemLogs = this.systemLogs;
-        console.log(this.systemLogs);
-        systemLogs.unshift(systemLog);
-        this.systemLogs = systemLogs;
+        var allSystemLogs = this.allSystemLogs;
+        console.log(this.allSystemLogs);
+        allSystemLogs.unshift(systemLog);
+        this.allSystemLogs = allSystemLogs;
     }
     
 }
